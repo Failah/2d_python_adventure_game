6f2ddaf41fac0a413b96282ca5b6fba680775873
ValerioScharizza <valerio.schiarizza@live.it>
2023-04-17 12:12:48 +0200

    added rocks

--- level.py
+++ level.py
@@ -1,4 +1,8 @@
 import pygame
+from tile import Tile
+from player import Player
+from settings import *
+
 
 class Level:
     def __init__(self):
@@ -8,6 +12,17 @@ class Level:
         self.visible_sprites = pygame.sprite.Group()
         self.obstacle_sprites = pygame.sprite.Group()
 
+        # sprite setup
+        self.create_map()
+
+    def create_map(self):
+        for row_index, row in enumerate(WORLD_MAP):
+            for col_index, col in enumerate(row):
+                x = col_index * TILESIZE
+                y = row_index * TILESIZE
+                if col == 'x':
+                    Tile((x, y), [self.visible_sprites])
+
     def run(self):
         # update and draw the game objects
-        pass
+        self.visible_sprites.draw(self.display_surface)
